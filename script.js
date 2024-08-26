a74ac5ceaab5c0f759dac631abdefab4baf71546
Sreeman Reddy <139124397+sremannn@users.noreply.github.com>
2024-08-26 11:13:59 +0530

    script.js

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ window.onpointermove = event => {
     top: `${clientY}px`
   }, { duration: 3000, fill: "forwards" });
 }
+
+const articles = document.querySelectorAll('article')
+
+const entry = video => () => {
+  video.currentTime = 0
+  video.play()
+}
+const exit = video => () => {
+  video.pause()
+}
+
+for (const article of articles) {
+  const video = article.querySelector('video')
+  video.addEventListener('loadeddata', () => article.dataset.loaded = true)
+  article.addEventListener('pointerenter', entry(video))
+  article.addEventListener('pointerleave', exit(video))
+}
